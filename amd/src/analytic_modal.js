70ed5b41fa352e9acef06763d5e4a28271fcea76
Tareq-Adnan <adnantareq2@gmail.com>
2024-07-11 13:09:46 +0600

    modal corner Radius

--- amd/src/analytic_modal.js
+++ amd/src/analytic_modal.js
@@ -48,11 +48,17 @@ export default class MyModal extends Modal {
         // Hide the default modal header.
         root.find('.modal-header').hide();
 
+        root.find('.modal-content').css({
+            'border-radius':'30px'
+        });
         // Remove padding from the modal content.
-        root.find('.modal-body').css('padding', '0');
+        root.find('.modal-body').css({
+            'padding':'0',
+            'border-radius':'30px'
+        });
         root.find('.modal-dialog').css({
             'max-width': '800px',
-            'min-height': '800px'
+            'min-height': '800px',
         });
 
         // Ensure modal closes on 'analytic-close' button click.
