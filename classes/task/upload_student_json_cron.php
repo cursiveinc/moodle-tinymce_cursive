17bda5da074df1d3deae7402b248445505abd5b0
Sha Md. Nawaz Sharif <Nawaz.Sharif@brainstation-23.com>
2024-07-31 19:23:53 +0600

    Update upload_student_json_cron.php

--- classes/task/upload_student_json_cron.php
+++ classes/task/upload_student_json_cron.php
@@ -90,6 +90,7 @@ class upload_student_json_cron extends \core\task\scheduled_task {
             }
           
             $uploaded = tiny_cursive_upload_multipart_record($filerecord, $filepath, $wstoken, $answer);
+            echo "#######################################\n";
             if ($uploaded) {
                 $filerecord->uploaded = strtotime(date('Y-m-d H:i:s'));
                 $DB->update_record($table, $filerecord);
