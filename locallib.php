7f233d466234b26e3a854e8cc170858bfed0ce39
Tareq-Adnan <adnantareq2@gmail.com>
2024-10-31 12:44:38 +0600

    updated download json file and removed require login from external func

--- locallib.php
+++ locallib.php
@@ -325,3 +325,22 @@ function create_token_for_user() {
     }
     return $token;
 }
+
+/**
+ * Method filestream
+ *
+ * @param $file $file [explicite description]
+ * @param $fname $fname [explicite description]
+ *
+ * @return string
+ */
+function file_stream($file, $fname) {
+    $inp = '';
+    if (file_exists($file)) {
+        $inp = file_get_contents($file);
+    } else {
+        $inp = base64_decode($file);
+    }
+
+    return $inp;
+}
