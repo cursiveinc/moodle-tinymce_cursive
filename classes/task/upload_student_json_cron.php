dad79c0be92bab6369c202208e92d218779eecd3
Tareq-Adnan <adnantareq2@gmail.com>
2025-02-20 13:49:00 +0600

    removed unnecessary code

--- classes/task/upload_student_json_cron.php
+++ classes/task/upload_student_json_cron.php
@@ -78,25 +78,6 @@ class upload_student_json_cron extends \core\task\scheduled_task {
         foreach ($filerecords as $filerecord) {
 
             $answer = $filerecord->original_content ?? "";
-            // if ($filerecord->modulename == 'quiz') {
-            //     $answer = tiny_cursive_get_user_essay_quiz_responses(
-            //         $filerecord->userid,
-            //         $filerecord->courseid,
-            //         $filerecord->resourceid,
-            //         $filerecord->modulename,
-            //         $filerecord->cmid,
-            //         $filerecord->questionid
-            //     );
-            // } else if ($filerecord->modulename == 'assign') {
-            //     $answer = tiny_cursive_get_user_onlinetext_assignments(
-            //         $filerecord->userid,
-            //         $filerecord->courseid,
-            //         $filerecord->modulename,
-            //         $filerecord->cmid
-            //     );
-            // } else if ($filerecord->modulename == 'forum') {
-            //     $answer = tiny_cursive_get_user_forum_posts($filerecord->userid, $filerecord->courseid, $filerecord->resourceid);
-            // }
 
             $uploaded = tiny_cursive_upload_multipart_record($filerecord, $filerecord->filename, $wstoken, $answer);
             if ($uploaded) {
