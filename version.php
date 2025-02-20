eb9cd9f19a625da128981752b5a0a50567bdfc5c
Sha Md. Nawaz Sharif <Nawaz.Sharif@brainstation-23.com>
2025-02-20 15:35:35 +0600

    Update version.php

--- version.php
+++ version.php
@@ -29,6 +29,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_cursive';
 $plugin->release = '0.1.2';
-$plugin->version = 2024060403;
+$plugin->version = 2024062003;
 $plugin->requires = 2022041912;
 $plugin->maturity = MATURITY_STABLE;
